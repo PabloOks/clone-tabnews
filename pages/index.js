f6b7680e621ab12b512ebfbe84e779152a8cec01
PabloOks <39108438+PabloOks@users.noreply.github.com>
2025-01-30 18:09:39 +0000

    add: `lint:check`, `lint:fix` and Prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Não desista de tentar, pois é assim que vai chegar ao sucesso</h1>
+  return <h1>Não desista de tentar, pois é assim que vai chegar ao sucesso</h1>;
 }
 
 export default Home;
